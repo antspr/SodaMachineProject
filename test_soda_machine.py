adc1e8b38f0acf565709044bc9563b2a8f67a405
antspr <antoinesprings@outlook.com>
2021-10-20 12:53:42 -0400

    Soda machine test class update

---
+++ test_soda_machine.py
@@ -0,0 +1,16 @@
+import unittest
+from soda_machine import SodaMachine
+
+class TestSodaMachine(unittest.TestCase):
+    "Test functions of Soda Machine"
+    def setUp(self):
+        self.sodamachine = SodaMachine()
+    
+    def test_fill_register(self):
+        pass
+
+
+
+
+if __name__ ==  'main.py':
+    unittest.main()
