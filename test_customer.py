940435e306659bfcdde16dc6f903ef1c56668c34
RaHughes <rogerallenhughes@hotmail.com>
2021-10-20 11:36:09 -0500

    Add tests for add_can_to_backpack method

--- test_customer.py
+++ test_customer.py
@@ -1,6 +1,7 @@
 import unittest
 from customer import Customer
 from coins import Dime
+from cans import Cola
 
 class TestGetWalletCoin(unittest.TestCase):
     """Test that each type of coin can be returned from Wallet"""
@@ -52,7 +53,29 @@ class TestAddCoinsToWallet(unittest.TestCase):
         self.customer.add_coins_to_wallet(coins_list)
         self.assertEqual (88, wallet_length_check)
 
-        
+class TestAddCanToBackpack(unittest.TestCase):
+    """Test that can can be added to backpack"""
+    def setUp(self):
+        self.customer = Customer()
+
+    def test_can_add_cola_to_backpack(self):
+        """Adds can to backpack"""
+        backpack_length_check = len(self.customer.backpack.purchased_cans)
+        self.customer.add_can_to_backpack(Cola())
+        self.assertEqual(1, backpack_length_check + 1)
+
+    def test_check_if_cola_name(self):
+        """Check if what was added is Cola""" 
+        self.customer.add_can_to_backpack(Cola())
+        can_name_check = self.customer.backpack.purchased_cans[0].name
+        self.assertEqual('Cola', can_name_check)
+    
+    def test_check_if_cola_price(self):
+        """Check if what was added is Cola's price""" 
+        self.customer.add_can_to_backpack(Cola())
+        can_price_check = self.customer.backpack.purchased_cans[0].price
+        self.assertEqual(.60, can_price_check)
+
 
 if __name__ == '__main__':
     unittest.main()
