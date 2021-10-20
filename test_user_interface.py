d78d82d98e4b550ed8f0db8c16d60813c23df54a
RaHughes <rogerallenhughes@hotmail.com>
2021-10-20 13:08:55 -0500

    Add structre for testing suite

---
+++ test_user_interface.py
@@ -0,0 +1,64 @@
+import unittest
+
+class TestValidateMainMenu(unittest.TestCase):
+    """"""
+
+    def test_ensure_tuple_one(self):
+        """"""
+        pass
+
+    def test_ensure_tuple_two(self):
+        """"""
+        pass
+
+    def test_ensure_tuple_three(self):
+        """"""
+        pass
+
+    def test_ensure_tuple_four(self):
+        """"""
+        pass
+
+    def test_ensure_invalid_false(self):
+        """"""
+        pass
+
+class TestTryParseInt(unittest.TestCase):
+    """"""
+
+    def test_valid_return(self):
+        """"""
+        pass
+
+    def test_invalid_return(self):
+        """"""
+        pass
+
+class TestGetUniqueCanNames(unittest.TestCase):
+    """"""
+
+    def test_remove_duplicate_name(self):
+        """"""
+        pass
+
+    def test_append_none_if_empty(self):
+        """"""
+        pass
+
+class TestDisplayPaymentValue(unittest.TestCase):
+    """"""
+
+    def test_proper_value_returned(self):
+        """"""
+        pass
+
+    def test_invalid_value_handled(self):
+        """"""
+        pass
+
+class TestValidateCoinSelection(unittest.TestCase):
+    """"""
+    pass
+
+if __name__ == '__main__':
+    unittest.main()
